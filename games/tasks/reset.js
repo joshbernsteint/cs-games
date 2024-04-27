9b55ce4d25ca8fd4506ff861d9ed3ce4ed5d54ab
Joshua Bernstein <joshuatbernstein@gmail.com>
2024-04-27 15:04:34 -0400

    changes

--- games/tasks/reset.js
+++ games/tasks/reset.js
@@ -1,7 +1,7 @@
 import { dbConnection, closeConnection } from "../config/mongoConnection.js";
 import { config } from "dotenv";
 import { createUser } from "../data/users.js";
-config({path: "../"});
+config();
 console.log(process.env);
 const db = await dbConnection();
 await db.dropDatabase();
